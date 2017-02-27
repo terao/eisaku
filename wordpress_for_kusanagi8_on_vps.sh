c1a9d6386ae95e322b7f5ca127748506a61244e7
terao <terao@readerror.info>
2017-02-27 20:22:52 +0900

    コメント更新、メールアドレスの正規表現削除、メール送信削除

--- wordpress_for_kusanagi8_on_vps.sh
+++ wordpress_for_kusanagi8_on_vps.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
-#   KUSANAGI8 環境に WordPress をセットアップするスクリプトです。
-#   このスクリプトを使うと kusanagi ユーザで ssh ログイン可能になります。
-#   サーバ作成後、WebブラウザでサーバのIPアドレスにアクセスしてください。
+#   さくらのVPSのKUSANAGI8 環境に WordPress をセットアップするスクリプトです。
+#   このスクリプトを使うと各種パスワードを自動生成します。
 #   https://サーバのIPアドレス/
 #   ※ セットアップには5〜10分程度時間がかかります。
-#   セットアップが正常に完了すると、 管理ユーザーのメールアドレス宛に完了メールが送付されます（お使いの環境によってはスパムフィルタにより受信されない場合があります）
-#   メール送信後、サーバを再起動をしますのメールを受信したら1分ほど待ってアクセスください。
-#   （このスクリプトは、KUSANAGI8.xでのみ動作します）
+#   （このスクリプトは、KUSANAGI8でのみ動作します）
 #   
 #   セットアップ後は、kusanagiのSSL(Let's Encrypt)設定や、WordPress のURL設定をIPアドレスからドメイン名に変更する設定の実施をおすすめします。
-#   詳細は詳細は以下のページを詳細は以下のページをご覧ください
-#    http://cloud-news.sakura.ad.jp/wordpress-for-kusanagi8/
-# @sacloud-desc-end
 # Command
 # 
 # curl -O https://raw.githubusercontent.com/terao/eisaku/master/wordpress_for_kusanagi8_on_vps.sh
 # 
 # chmod 700 wordpress_for_kusanagi8_on_vps.sh
 # 
-# ./wordpress_for_kusanagi8_on_vps.sh e-terao@sakura.ad.jp  2>&1 | tee -a wordpress_for_kusanagi8_on_vps.log
-# 
-# 
+# ./wordpress_for_kusanagi8_on_vps.sh terao@example.jp  2>&1 | tee -a wordpress_for_kusanagi8_on_vps.log
+# #(teeコマンドでログに出力するとパスワードをコピペできなかったときに便利です。必ず、パスワードをコピーしたらログを削除してください)
+#
+# # 最後にサーバのrebootを推奨します
 # reboot
 # 
-# 管理ユーザーの入力フォームの設定
-# @sacloud-password required shellarg maxlen=60 minlen=6 KUSANAGI_PASSWD  "ユーザー kusanagi のパスワード" ex="6〜60文字"
-# @sacloud-password required shellarg maxlen=60 minlen=6 DBROOT_PASSWD    "MariaDB root ユーザーのパスワード" ex="6〜60文字"
-# @sacloud-text     required shellarg maxlen=60 WP_ADMIN_USER    "WordPress 管理者ユーザ名 (半角英数字、下線、ハイフン、ピリオド、アットマーク (@) のみが使用可能)" ex="1〜60文字"
-# @sacloud-password required shellarg maxlen=60 minlen=6 WP_ADMIN_PASSWD  "WordPress 管理者パスワード" ex="6〜60文字"
-# @sacloud-text     required shellarg maxlen=256 WP_TITLE        "WordPress サイトのタイトル (256文字以下)"
-# @sacloud-text     required  maxlen=128 WP_ADMIN_MAIL   "WordPress 管理者メールアドレス (インストール完了時にメールが送信されます)" ex="user@example.com"
 
 echo "## start date";
 date
@@ -43,14 +31,6 @@ WP_ADMIN_USER="admin_`mkpasswd -l 5 -C 0 -s 0`"
 WP_ADMIN_PASSWD=`mkpasswd -l 32 -d 9 -c 9 -C 9 -s 0 -2`
 WP_TITLE="ICHIGEKI WordPress on KUSANAGI"
 WP_ADMIN_MAIL="$1"
-
-#if [[ "$email" =~ ^[A-Za-z0-9_%+\.\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,4}$ ]] ;
-#then 
-#    echo "Email address $WP_ADMIN_MAIL is valid."
-#else
-#    echo "Email address $WP_ADMIN_MAIL is invalid."
-#    exit 1;
-#fi
 
 TERM=xterm
 
@@ -133,36 +113,16 @@ sudo -u kusanagi  -i /usr/local/bin/wp core install \
 
 #---------END OF WordPrss---------#
 
-# いつ完了しているか分からないため、WP管理者メールアドレスに完了メールを送信
-
-# 必要な情報を集める
-SYSTEMINFO=`dmidecode -t system`
-
-# フォームで設定した管理者のアドレスへメールを送信
-echo "## send email.";
-/usr/sbin/sendmail -t -i -o -f $WP_ADMIN_MAIL << EOF From: $WP_ADMIN_MAIL
-Subject: finished Wordpress install on $IPADDRESS0
-To: $WP_ADMIN_MAIL
-
-Finish WordPress install on $IPADDRESS0
-
-Please access to https://$IPADDRESS0 after 1 min.
-
-Autogenerate DBname, DBUsername, DBpassword etc...
-You can find Wordpress DB Infomation in /home/kusanagi/default_profile/DocumentRoot/wp-config.php
-
-System Info:
-$SYSTEMINFO
-EOF
 
 echo "## finished!";
 
 
 cat << EOS
-- Server Infomation
-Web Server : Nginx
-PHP Type   : hhvm
-IP Address : $IPADDRESS0
+- KUSANAGI Server Infomation
+Web Server      : Nginx
+PHP Type        : hhvm
+Default Profile : default_profile
+IP Address      : $IPADDRESS0
 
 - Linux User Infomation
 kusanagi user Password     : $KUSANAGI_PASSWD
@@ -171,6 +131,8 @@ kusanagi user Password     : $KUSANAGI_PASSWD
 MariaDB root Password      : $DBROOT_PASSWD
 MariaDB Wordpress Username : $WPDB_USERNAME
 MariaDB Wordpress Password : $WPDB_PASSWORD
+MariaDB Host               : localhost
+MariaDB Charset            : utf8mb4
 
 - WordPress Infomation
 Wordpress URL              : https://$IPADDRESS0/
@@ -178,6 +140,7 @@ Wordpress Admin Username   : $WP_ADMIN_USER
 Wordpress Admin Password   : $WP_ADMIN_PASSWD
 Wordpress Title            : $WP_TITLE
 Wordpress Admin Email      : $WP_ADMIN_MAIL
+Document Root              : /home/kusanagi/default_profile/DocumentRoot/
 
 EOS
 
